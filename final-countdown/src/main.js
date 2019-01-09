53a811576fb19f1837b12c28682f8713e0137a67
Dina <dcaraballo26@gmail.com>
2019-01-08 20:58:15 -0700

    Add Gallery component

--- final-countdown/src/main.js
+++ final-countdown/src/main.js
@@ -4,6 +4,29 @@ import { apikey } from '../apikey'
 
 Vue.config.productionTip = false
 
+const baseUrl = "https://api.harvardartmuseums.org"
+
+const Gallery = {
+  template: '#gallery-template',
+  data: () => ({
+    posts: [],
+    images: [],
+    search: ""
+  }),
+  mounted() {
+    this.getPosts()
+  },
+  methods: {
+    getPosts() {
+      axios.get(`${baseUrl}/image?apikey=d4537190-135f-11e9-baea-f75a4720f151`).then(response => {
+        this.posts = response.data.records
+        this.posts.forEach(post => console.log(post.baseimageurl))
+      })
+    }
+  }
+}
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
